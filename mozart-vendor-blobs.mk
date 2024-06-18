c6177d871e6ad664734c2ab83c166cccccc63e9f
schwienernitzel <pfelix0803@gmail.com>
2024-06-18 21:35:55 +0000

    mozart: Add oeminfo and tee blobs
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- mozart-vendor-blobs.mk
+++ mozart-vendor-blobs.mk
@@ -13,6 +13,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+VENDOR_PATH := vendor/huawei/mozart/proprietary
+
 # Blobs
 PRODUCT_COPY_FILES += \
-	$(call find-copy-subdir-files,*,vendor/huawei/mozart/proprietary/,system/)
+    $(VENDOR_PATH)/lib/lib_nve_operation.so:/system/lib/lib_nve_operation.so \
+    $(VENDOR_PATH)/lib/lib_oeminfo_interface.so:/system/lib/lib_oeminfo_interface.so \
+    $(VENDOR_PATH)/lib/libnvme.so:/system/lib/libnvme.so \
+    $(VENDOR_PATH)/lib/liboeminfo.so:/system/lib/liboeminfo.so \
+    $(VENDOR_PATH)/lib/liboeminfo_jni.so:/system/lib/liboeminfo_jni.so \
+    $(VENDOR_PATH)/lib/libteec.so:/system/lib/libteec.so \
+    $(VENDOR_PATH)/lib64/lib_nve_operation.so:/system/lib64/lib_nve_operation.so \
+    $(VENDOR_PATH)/lib64/lib_oeminfo_interface.so:/system/lib64/lib_oeminfo_interface.so \
+    $(VENDOR_PATH)/lib64/libnvme.so:/system/lib64/libnvme.so \
+    $(VENDOR_PATH)/lib64/liboeminfo.so:/system/lib64/liboeminfo.so \
+    $(VENDOR_PATH)/lib64/liboeminfo_jni.so:/system/lib64/liboeminfo_jni.so \
+    $(VENDOR_PATH)/lib64/libteec.so:/system/lib64/libteec.so
