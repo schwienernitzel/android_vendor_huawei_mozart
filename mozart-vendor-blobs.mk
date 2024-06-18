a3646dbb0011e097151d88edf3b3e3e54ceb9041
schwienernitzel <pfelix0803@gmail.com>
2024-06-18 21:44:29 +0000

    mozart: Add mali blobs
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- mozart-vendor-blobs.mk
+++ mozart-vendor-blobs.mk
@@ -28,4 +28,12 @@ PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/lib64/libnvme.so:/system/lib64/libnvme.so \
     $(VENDOR_PATH)/lib64/liboeminfo.so:/system/lib64/liboeminfo.so \
     $(VENDOR_PATH)/lib64/liboeminfo_jni.so:/system/lib64/liboeminfo_jni.so \
-    $(VENDOR_PATH)/lib64/libteec.so:/system/lib64/libteec.so
+    $(VENDOR_PATH)/lib64/libteec.so:/system/lib64/libteec.so \
+    $(VENDOR_PATH)/lib/hw/copybit.hi3635.so:/system/lib/hw/copybit.hi3635.so \
+    $(VENDOR_PATH)/lib/hw/gralloc.hi3635.so:/system/lib/hw/gralloc.hi3635.so \
+    $(VENDOR_PATH)/lib/libion.so:/system/lib/libion.so \
+    $(VENDOR_PATH)/lib64/hw/copybit.hi3635.so:/system/lib64/hw/copybit.hi3635.so \
+    $(VENDOR_PATH)/lib64/hw/gralloc.hi3635.so:/system/lib64/hw/gralloc.hi3635.so \
+    $(VENDOR_PATH)/lib64/libion.so:/system/lib64/libion.so \
+    $(VENDOR_PATH)/vendor/lib/egl/libGLES_mali.so:/system/lib/egl/libGLES_mali.so \
+    $(VENDOR_PATH)/vendor/lib64/egl/libGLES_mali.so:/system/lib64/egl/libGLES_mali.so
