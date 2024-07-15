990fc90b222cc3151e7f8f52e6271e495de8d035
schwienernitzel <pfelix0803@gmail.com>
2024-07-15 13:18:55 +0000

    mozart: Drop depreceated liblog shims
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- mozart-vendor-blobs.mk
+++ mozart-vendor-blobs.mk
@@ -397,8 +397,6 @@ PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/vendor/lib64/libbt-vendor.so:/system/vendor/lib64/libbt-vendor.so \
     $(VENDOR_PATH)/lib/libaes.so:/system/lib/libaes.so \
     $(VENDOR_PATH)/lib64/libaes.so:/system/lib64/libaes.so \
-    $(VENDOR_PATH)/lib/liblog.so:/system/lib/libshim_log.so \
-    $(VENDOR_PATH)/lib64/liblog.so:/system/lib64/libshim_log.so \
     $(VENDOR_PATH)/lib/libomx_vxd.so:/system/lib/libomx_vxd.so \
     $(VENDOR_PATH)/lib/libomx_vxe.so:/system/lib/libomx_vxe.so \
     $(VENDOR_PATH)/lib/libstagefrighthw.so:/system/lib/libstagefrighthw.so \
